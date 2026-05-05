be14a0c9fb961b7a31f8b9144b7c2ba9712195a9
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-05 12:54:02 -0400

    Create express app

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = require('./routes/index');
+
+app.use(express.json());
+
+app.use('/api', router);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
