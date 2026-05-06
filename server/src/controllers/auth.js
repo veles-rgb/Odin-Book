0309d2190fa7c7a8bbb08d807a5405f23346f417
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-06 15:49:52 -0400

    Add refresh token functionality
    Create createAccessToken and add creation of refresh tokens on login

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -3,6 +3,24 @@ const jwt = require('jsonwebtoken');
 
 const { prisma } = require('../../lib/prisma.mjs');
 
+function generateAccessToken(user) {
+    return jwt.sign(
+        {
+            sub: user.id,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '15s' }
+    );
+}
+
+function generateRefreshToken(user) {
+    return jwt.sign(
+        { sub: user.id },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "7d" }
+    );
+}
+
 async function registerUser(req, res, next) {
     try {
         const { first_name, last_name, username, password } = req.body;
@@ -53,16 +71,27 @@ async function loginUser(req, res, next) {
 
         if (!ok) return res.status(401).json({ error: 'Invalid credentials' });
 
-        const accessToken = jwt.sign(
-            {
-                sub: user.id,
-                username: user.username,
-                firstName: user.first_name,
-                lastName: user.last_name
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '15m' }
-        );
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+
+        const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+        const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+        await prisma.refreshToken.create({
+            data: {
+                token_hash: hashedRefreshToken,
+                user_id: user.id,
+                expires_at: expiresAt
+            }
+        });
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            path: "/api/auth",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
 
         return res.json({ accessToken: accessToken });
 
@@ -71,7 +100,60 @@ async function loginUser(req, res, next) {
     }
 }
 
+async function createAccessToken(req, res, next) {
+    try {
+        const refreshToken = req.cookies?.refreshToken;
+
+        if (!refreshToken) return res.status(401).json({ error: 'Refresh token required' });
+
+        // 1. verify refresh token
+        const payload = jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        );
+
+        // 2. check it against DB
+        const storedToken = await prisma.refreshToken.findMany({
+            where: {
+                user_id: payload.sub
+            }
+        });
+
+        if (!storedToken) return res.status(403).json({ error: "Invalid refresh token" });
+
+        let validToken = null;
+
+        for (const token of storedToken) {
+            const match = await bcrypt.compare(
+                refreshToken,
+                token.token_hash
+            );
+
+            if (match) {
+                validToken = token;
+                break;
+            }
+        }
+
+        if (!validToken) {
+            return res.status(403).json({ error: "Invalid refresh token" });
+        }
+
+        // 3. create new access token
+        const accessToken = jwt.sign(
+            { sub: payload.sub },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '15s' }
+        );
+
+        return res.json({ accessToken });
+    } catch (error) {
+        return res.status(403).json({ error: 'Invalid refresh token' });
+    }
+}
+
 module.exports = {
     registerUser,
     loginUser,
+    createAccessToken
 };
