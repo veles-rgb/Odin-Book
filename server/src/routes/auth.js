30c1326020342da16b7ac290d7c28aa1ef0728f7
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-06 13:26:28 -0400

    Create /register and /login routes

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
+const controller = require('../controllers/auth');
 
 // get /verify (requireAuth)
-// post /register
-// post /login
+router.post('/register', controller.registerUser);
+router.post('/login', controller.loginUser);
 
 module.exports = router;
