1881aa7c3a35e8041eb06b2e50e93befb2f47188
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-06 15:52:38 -0400

    Use CookieParser

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./routes/index');
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api', router);
 
