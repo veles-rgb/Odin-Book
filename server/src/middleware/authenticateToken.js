6db80cb8118e170cd9fc4520e6c08356734ee8ba
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-07 14:13:08 -0400

    Update req.user to have id instead of sub

--- server/src/middleware/authenticateToken.js
+++ server/src/middleware/authenticateToken.js
@@ -8,7 +8,9 @@ function authenticateToken(req, res, next) {
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
-        req.user = user;
+        req.user = {
+            id: user.sub,
+        };
         next();
     });
 }
