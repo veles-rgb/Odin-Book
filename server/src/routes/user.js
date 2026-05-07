202d82ffc3f4b4729ab7ee66386b4adbba708159
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-07 14:13:42 -0400

    Create searchUser, getUser & updateUser routes

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const controller = require('../controllers/user');
 
-router.use('/:identifier', controller.getUser);
+router.get('/', controller.searchUser);
+router.get('/:identifier', controller.getUser);
+router.patch('/update/:id', controller.updateUser);
 
 module.exports = router;
