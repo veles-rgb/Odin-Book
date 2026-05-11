735c980cda4b83cda6cf64e6fd6a8487e7d1f2ad
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-11 15:44:03 -0400

    Create post/:id route for getPostById

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('../controllers/post');
 
+router.get('/:id', controller.getPostById);
 router.post('/create', controller.createPost);
 router.patch('/edit/:id', controller.editPost);
 router.delete("/delete/:id", controller.deletePost);
