640bd1a230ad36d8e830280b62d0c83e99c22488
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-11 15:03:09 -0400

    Create post/delete/:id route

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/post');
 
 router.post('/create', controller.createPost);
 router.patch('/edit/:id', controller.editPost);
+router.delete("/delete/:id", controller.deletePost);
 
 module.exports = router;
