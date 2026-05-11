0b3a1130ef0f68e53cb97dcca513799c475b8ed3
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-11 14:42:16 -0400

    Create post router with /create & /edit routes

---
+++ server/src/routes/post.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const controller = require('../controllers/post');
+
+router.post('/create', controller.createPost);
+router.patch('/edit/:id', controller.editPost);
+
+module.exports = router;
