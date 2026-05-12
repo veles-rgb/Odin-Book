0084eb06b516529e35acb3ece48ab1c269c307f0
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-12 15:12:22 -0400

    Add reply to comment route

--- server/src/routes/comment.js
+++ server/src/routes/comment.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/comment');
 router.post('/create/:id', controller.createComment);
 router.patch('/edit/:id', controller.editComment);
 router.delete('/delete/:id', controller.deleteComment);
+router.post('/reply/:id', controller.createReplyComment);
 
 module.exports = router;
