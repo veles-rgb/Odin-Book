01f8856f58291dff38b877c4e78cbcf292ee0aee
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-12 14:38:44 -0400

    Create delete comment route

--- server/src/routes/comment.js
+++ server/src/routes/comment.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/comment');
 
 router.post('/create/:id', controller.createComment);
 router.patch('/edit/:id', controller.editComment);
+router.delete('/delete/:id', controller.deleteComment);
 
 module.exports = router;
