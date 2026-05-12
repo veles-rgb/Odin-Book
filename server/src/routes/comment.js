1a5ac36a338ff4f30fcf8a1614c5fdaff194ea4b
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-12 14:10:19 -0400

    Create edit comment route

--- server/src/routes/comment.js
+++ server/src/routes/comment.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/comment');
 
 router.post('/create/:id', controller.createComment);
+router.patch('/edit/:id', controller.editComment);
 
 module.exports = router;
