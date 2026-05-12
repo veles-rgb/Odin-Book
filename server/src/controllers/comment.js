1d894ce3f265ec02b2762ef37b05df402af8b849
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-12 14:10:43 -0400

    Create edit comment controller function

--- server/src/controllers/comment.js
+++ server/src/controllers/comment.js
@@ -49,7 +49,58 @@ async function createComment(req, res, next) {
     }
 }
 
+async function editComment(req, res, next) {
+    try {
+        const user = req.user;
+        const { id } = req.params;
+        const { content } = req.body;
+
+        const trimmedContent = content.trim();
+        if (!isUUID(id)) return res.status(400).json({ error: "Invalid Comment ID." });
+        if (!trimmedContent) return res.status(400).json({ error: "Comment cannot be blank." });
+
+        const comment = await prisma.comment.findFirst({
+            where: {
+                id,
+            },
+        });
+
+        if (!comment) return res.status(404).json({ error: "Comment does not exist." });
+
+        const updatedComment = await prisma.comment.update({
+            where: {
+                id,
+                user_id: user.id,
+            },
+            data: {
+                content: trimmedContent,
+            },
+            select: {
+                id: true,
+                post_id: true,
+                user_id: true,
+                content: true,
+                created_at: true,
+                user: {
+                    select: {
+                        id: true,
+                        first_name: true,
+                        last_name: true,
+                        username: true,
+                        profile_picture_url: true,
+                    },
+                },
+            },
+        });
+
+        return res.status(200).json({ updatedComment });
+
+    } catch (error) {
+        return next(error);
+    }
+}
+
 module.exports = {
     createComment,
-
+    editComment,
 };
