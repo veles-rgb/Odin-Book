40ffff76516a49bab88cf7d32e0f50978266d00f
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-12 15:12:40 -0400

    Add reply to comment controller function

--- server/src/controllers/comment.js
+++ server/src/controllers/comment.js
@@ -55,9 +55,25 @@ async function editComment(req, res, next) {
         const { id } = req.params;
         const { content } = req.body;
 
+        if (!isUUID(id)) {
+            return res.status(400).json({
+                error: "Invalid Comment ID.",
+            });
+        }
+
+        if (!content) {
+            return res.status(400).json({
+                error: "Comment cannot be blank.",
+            });
+        }
+
         const trimmedContent = content.trim();
-        if (!isUUID(id)) return res.status(400).json({ error: "Invalid Comment ID." });
-        if (!trimmedContent) return res.status(400).json({ error: "Comment cannot be blank." });
+
+        if (!trimmedContent) {
+            return res.status(400).json({
+                error: "Comment cannot be blank.",
+            });
+        }
 
         const comment = await prisma.comment.findFirst({
             where: {
@@ -65,12 +81,25 @@ async function editComment(req, res, next) {
             },
         });
 
-        if (!comment) return res.status(404).json({ error: "Comment does not exist." });
+        if (!comment) {
+            return res.status(404).json({
+                error: "Comment does not exist.",
+            });
+        }
+
+        if (comment.user_id !== user.id) {
+            return res.sendStatus(403);
+        }
+
+        if (comment.content === trimmedContent) {
+            return res.status(400).json({
+                error: "No changes were made.",
+            });
+        }
 
         const updatedComment = await prisma.comment.update({
             where: {
                 id,
-                user_id: user.id,
             },
             data: {
                 content: trimmedContent,
@@ -129,8 +158,58 @@ async function deleteComment(req, res, next) {
     }
 }
 
+async function createReplyComment(req, res, next) {
+    try {
+        const user = req.user;
+        const { id } = req.params; // comment id
+        const { content } = req.body;
+
+        const trimmedContent = content.trim();
+        if (!isUUID(id)) return res.status(400).json({ error: "Invalid post ID." });
+        if (!trimmedContent) return res.status(400).json({ error: "Reply comment cannot be blank." });
+
+        const parentComment = await prisma.comment.findFirst({
+            where: {
+                id,
+            },
+        });
+        if (!parentComment) return res.status(404).json({ error: "The comment you're trying to reply to either doesn't exist or was removed." });
+
+        const comment = await prisma.comment.create({
+            data: {
+                content: trimmedContent,
+                parent_id: parentComment.id,
+                post_id: parentComment.post_id,
+                user_id: user.id,
+            },
+            select: {
+                id: true,
+                post_id: true,
+                parent_id: true,
+                created_at: true,
+                content: true,
+                user: {
+                    select: {
+                        id: true,
+                        first_name: true,
+                        last_name: true,
+                        username: true,
+                        profile_picture_url: true
+                    },
+                },
+            },
+        });
+
+        return res.status(201).json({ comment });
+
+    } catch (error) {
+        return next(error);
+    }
+}
+
 module.exports = {
     createComment,
     editComment,
     deleteComment,
+    createReplyComment,
 };
