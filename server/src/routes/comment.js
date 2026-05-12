29dc6392e97a0b093cbb107bf6eb0c489c9fb4ef
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-12 15:41:58 -0400

    Create get comment by id route

--- server/src/routes/comment.js
+++ server/src/routes/comment.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('../controllers/comment');
 
+router.get('/:id', controller.getCommentById);
 router.post('/create/:id', controller.createComment);
 router.patch('/edit/:id', controller.editComment);
 router.delete('/delete/:id', controller.deleteComment);
