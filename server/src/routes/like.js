673c631287a1143072739baf2a96ffb58b3992dc
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-13 15:51:00 -0400

    Add create and delete post like routes

--- server/src/routes/like.js
+++ server/src/routes/like.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('../controllers/like');
 
-
+router.post('/post/:id', controller.createPostLike);
+router.delete('/post/:id', controller.deletePostLike);
 
 module.exports = router;
