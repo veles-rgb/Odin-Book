a6512b557187f02df5115eacb81e57af17f97900
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-13 15:51:34 -0400

    Add createPostLike & deletePostLike controller functions

--- server/src/controllers/like.js
+++ server/src/controllers/like.js
@@ -1,6 +1,86 @@
 const { prisma } = require('../../lib/prisma.mjs');
 const { validate: isUUID } = require("uuid");
 
-module.exports = {
+// Posts
+async function createPostLike(req, res, next) {
+    try {
+        const { id } = req.params;
+        const user = req.user;
 
+        if (!isUUID(id)) return res.status(400).json({ error: "Invalid post ID." });
+
+        const post = await prisma.post.findFirst({
+            where: {
+                id,
+            },
+        });
+
+        if (!post) return res.status(404).json({ error: "Post not found." });
+
+        const alreadyLiked = await prisma.postLike.findFirst({
+            where: {
+                post_id: id,
+                user_id: user.id,
+            },
+        });
+
+        if (alreadyLiked) return res.status(400).json({ error: "You already liked this post." });
+
+        const postLike = await prisma.postLike.create({
+            data: {
+                post_id: id,
+                user_id: user.id,
+            },
+        });
+
+        return res.status(201).json({ postLike });
+    } catch (error) {
+        return next(error);
+    }
+}
+
+async function deletePostLike(req, res, next) {
+    try {
+        const user = req.user;
+        const { id } = req.params;
+
+        if (!isUUID(id)) return res.status(400).json({ error: "Invalid post ID." });
+
+        const post = await prisma.post.findFirst({
+            where: {
+                id,
+            },
+        });
+
+        if (!post) return res.status(404).json({ error: "Post not found." });
+
+        const like = await prisma.postLike.findFirst({
+            where: {
+                post_id: post.id,
+                user_id: user.id,
+            },
+        });
+
+        if (!like) return res.status(404).json({ error: "You have not liked this post." });
+
+        const deletedLike = await prisma.postLike.delete({
+            where: {
+                user_id_post_id: {
+                    user_id: user.id,
+                    post_id: post.id,
+                },
+            },
+        });
+
+        return res.status(200).json({ deletedLike });
+    } catch (error) {
+        return next(error);
+    }
+}
+
+// Comments
+
+module.exports = {
+    createPostLike,
+    deletePostLike,
 };
