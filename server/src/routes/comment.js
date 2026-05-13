68b77693af4a9cbad26e46cee06fcb783150c8d1
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-13 14:21:09 -0400

    Add /comment/:id/replies route
    For fetching replies to comments

--- server/src/routes/comment.js
+++ server/src/routes/comment.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const controller = require('../controllers/comment');
 
 router.get('/:id', controller.getCommentById);
+router.get('/:id/replies', controller.getCommentReplies);
 router.post('/create/:id', controller.createComment);
 router.patch('/edit/:id', controller.editComment);
 router.delete('/delete/:id', controller.deleteComment);
