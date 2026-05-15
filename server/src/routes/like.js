c5786c51124d2c9c00fa3805803fba7e6121c900
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-15 14:46:00 -0400

    Add create & delete comment like routes

--- server/src/routes/like.js
+++ server/src/routes/like.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const controller = require('../controllers/like');
 
 router.post('/post/:id', controller.createPostLike);
+router.post('/comment/:id', controller.createCommentLike);
 router.delete('/post/:id', controller.deletePostLike);
+router.delete('/comment/:id', controller.deleteCommentLike);
+
 
 module.exports = router;
