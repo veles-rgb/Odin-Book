fc363c75378bed0ef9485ddd894e663e3cc2720a
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-19 15:47:14 -0400

    Create routes for /follow

--- server/src/routes/follow.js
+++ server/src/routes/follow.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
 const controller = require('../controllers/follow');
 
+router.post('/:id/request', controller.sendFollowRequest); // sendFollowRequest
+// router.delete('/:id/request'); // cancelFollowRequest
+
+// router.post('/requests/:id/accept'); // acceptFollowRequest
+// router.delete('/requests/:id/reject'); // rejectFollowRequest
+
+// router.get('/requests/received'); // getReceivedFollowRequests
+// router.get('/requests/sent'); // getSentFollowRequests
+
+// router.delete('/:id'); // unfollowUser
+
+// router.get('/:id/followers'); // getFollowers
+// router.get('/:id/following'); // getFollowing
 
 module.exports = router;
