d6cf40717b842fab2e3ef86f6318ddb67f9c7e0a
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-19 15:47:31 -0400

    Add sendFollowRequest router controller function

--- server/src/controllers/follow.js
+++ server/src/controllers/follow.js
@@ -1,2 +1,63 @@
 const { prisma } = require('../../lib/prisma.mjs');
 const { validate: isUUID } = require("uuid");
+
+async function sendFollowRequest(req, res, next) {
+    try {
+        const user = req.user;
+        const { id } = req.params;
+
+        if (!isUUID(id)) return res.status(400).json({ error: "Invalid user ID." });
+
+        const requestedUser = await prisma.user.findFirst({
+            where: {
+                id,
+            },
+        });
+
+        if (!requestedUser) return res.status(404).json({ error: "That user does not exist." });
+
+        if (requestedUser.id === user.id) return res.status(400).json({ error: "You cannot follow yourself." });
+
+        // Check if already requested or following
+        const checkAlreadyFollowing = await prisma.follow.findFirst({
+            where: {
+                follower_id: user.id,
+                following_id: requestedUser.id,
+            },
+        });
+
+        if (checkAlreadyFollowing) return res.status(409).json({ error: "You already follow this user." });
+
+        const checkAlreadyRequested = await prisma.followRequest.findFirst({
+            where: {
+                requester_id: user.id,
+                receiver_id: requestedUser.id,
+            },
+        });
+
+        if (checkAlreadyRequested) return res.status(409).json({ error: "You already requested to follow this user." });
+
+        const followRequest = await prisma.followRequest.create({
+            data: {
+                requester_id: user.id,
+                receiver_id: requestedUser.id,
+            },
+        });
+
+        return res.status(201).json({ follow_request: followRequest });
+    } catch (error) {
+        if (error.code === "P2002") {
+            return res.status(409).json({
+                error: "You already requested to follow this user.",
+            });
+        }
+
+        return next(error);
+    }
+}
+
+
+module.exports = {
+    sendFollowRequest,
+
+};
