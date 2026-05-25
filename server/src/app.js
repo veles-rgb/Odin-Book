c64bee793a975138cbff065efe0c2661da6fe048
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-25 18:15:14 -0400

    Enable cors

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./routes/index');
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
