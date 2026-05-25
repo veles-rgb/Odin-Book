5b8f8dec7c283f35b5348d43401e6bdeb8d843dc
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-25 19:07:55 -0400

    Import navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Pages and Components
+import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
