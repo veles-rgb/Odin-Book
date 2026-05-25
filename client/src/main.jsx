0aa415f36678729c2c23f9882cf5249335f4c4e0
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-25 16:42:41 -0400

    Wrap app with AuthContextProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { AuthContextProvider } from './context/AuthContext.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </StrictMode>,
-)
+);
