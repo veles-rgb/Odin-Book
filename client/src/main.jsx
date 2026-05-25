62817a51154553aadfaf985fb5efddd95fa13f7c
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-25 17:23:37 -0400

    Update AuthContextProvider import location

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { AuthContextProvider } from './context/AuthContext.js';
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
