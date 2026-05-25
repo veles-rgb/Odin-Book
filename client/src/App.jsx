668e49b94f46264e9328c9264ad4852e04267682
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-25 17:24:12 -0400

    Add /register and /login routes
    Using react-router-dom's BrowserRouter, Routes & Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages and Components
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
