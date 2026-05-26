1b47c770da8ce26daacfa903a7e2bfc2e630aceb
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-05-26 13:24:23 -0400

    Add conditional page rendering based on Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 // Pages and Components
 import Navbar from './components/Navbar';
@@ -7,13 +8,21 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/register"
+            element={user ? <Navigate to={'/'} /> : <Register />}
+          />
+          <Route
+            path="/login"
+            element={user ? <Navigate to={'/'} /> : <Login />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
