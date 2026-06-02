b0cbc751d32d04deceafece3b177421d93bced93
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-06-02 14:28:23 -0400

    Add post feed to home page with create post btn

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,104 @@
-const Home = () => {};
+import { useState, useEffect } from 'react';
+import { useApiFetch } from '../hooks/useApiFetch';
+import PostCard from '../components/PostCard';
+import CreatePostModal from '../components/CreatePostModal';
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const [showCreatePost, setShowCreatePost] = useState(false);
+
+  const { apiFetch } = useApiFetch();
+
+  const fetchPosts = async (pageToFetch) => {
+    try {
+      setIsLoading(true);
+      setError(null);
+
+      const response = await apiFetch(
+        `/api/post/feed?page=${pageToFetch}&limit=10`,
+      );
+
+      if (!response) return;
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError(data.error);
+        return;
+      }
+
+      if (pageToFetch === 1) {
+        setPosts(data.posts);
+      } else {
+        setPosts((prev) => [...prev, ...data.posts]);
+      }
+
+      setHasNextPage(data.hasNextPage);
+      // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      setError('Something went wrong.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    fetchPosts(1);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleShowCreatePost = () => {
+    setShowCreatePost((prev) => !prev);
+  };
+
+  const handleShowMore = async () => {
+    const nextPage = page + 1;
+
+    await fetchPosts(nextPage);
+
+    setPage(nextPage);
+  };
+
+  return (
+    <main>
+      <div>
+        <button type="button" onClick={handleShowCreatePost}>
+          Create Post
+        </button>
+
+        {showCreatePost && (
+          <CreatePostModal
+            onClose={() => setShowCreatePost(false)}
+            onPostCreated={(newPost) => {
+              setPosts((prev) => [newPost, ...prev]);
+            }}
+          />
+        )}
+      </div>
+
+      {error && <div>{error}</div>}
+
+      <div>
+        {posts.map((post) => (
+          <PostCard post={post} key={post.id} />
+        ))}
+      </div>
+
+      {hasNextPage && (
+        <button type="button" onClick={handleShowMore} disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Show More'}
+        </button>
+      )}
+    </main>
+  );
+};
 
 export default Home;
