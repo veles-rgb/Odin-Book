4da404f48c9fd91869ece957cd710528c4bdfe10
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-06-03 14:28:49 -0400

    Create OptionsMenu component

---
+++ client/src/components/OptionsMenu.jsx
@@ -0,0 +1,44 @@
+import { useState, useRef, useEffect } from 'react';
+import styles from './OptionsMenu.module.css';
+import { HiDotsVertical } from 'react-icons/hi';
+
+const OptionsMenu = ({ className = '', children }) => {
+  const menuRef = useRef(null);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = () => {
+    setShowMenu((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const handleOutsideClick = (e) => {
+      if (showMenu && !menuRef.current?.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [showMenu]);
+
+  return (
+    <div className={`${styles.wrapper} ${className}`}>
+      <button type="button" className={styles.trigger} onClick={handleClick}>
+        <HiDotsVertical />
+      </button>
+
+      {showMenu && (
+        <div ref={menuRef} className={styles.menu}>
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OptionsMenu;
