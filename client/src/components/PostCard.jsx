a4e4bb94de1dc17f681c10de6a9ce817790823c3
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-06-08 16:16:30 -0400

    Add links to user profile

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -35,17 +35,21 @@ const PostCard = ({
   return (
     <article className={styles.card}>
       <div className={styles.header}>
-        <img
-          className={styles.avatar}
-          src={
-            post.user.profile_picture_url ||
-            'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'
-          }
-          alt={`${post.user.username}'s profile picture`}
-        />
+        <a href={`/profile/${user.id}`}>
+          <img
+            className={styles.avatar}
+            src={
+              post.user.profile_picture_url ||
+              'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'
+            }
+            alt={`${post.user.username}'s profile picture`}
+          />
+        </a>
 
         <div className={styles.userInfo}>
-          <div className={styles.username}>{post.user.username}</div>
+          <a href={`/profile/${user.id}`}>
+            <div className={styles.username}>{post.user.username}</div>
+          </a>
 
           <div className={styles.dateTimeContainer}>
             <div className={styles.timestamp}>
