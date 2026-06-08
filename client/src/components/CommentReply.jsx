cb42b5ec2211d084176f075a063e0ae1571489bc
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-06-08 16:16:06 -0400

    Add links to user profile

--- client/src/components/CommentReply.jsx
+++ client/src/components/CommentReply.jsx
@@ -72,17 +72,21 @@ const CommentReply = ({
   return (
     <article className={styles.reply}>
       <div className={styles.header}>
-        <img
-          className={styles.avatar}
-          src={
-            comment.user.profile_picture_url ||
-            'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'
-          }
-          alt={`${comment.user.username}'s profile picture`}
-        />
+        <a href={`/profile/${user.id}`}>
+          <img
+            className={styles.avatar}
+            src={
+              comment.user.profile_picture_url ||
+              'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'
+            }
+            alt={`${comment.user.username}'s profile picture`}
+          />
+        </a>
 
         <div className={styles.userInfo}>
-          <div className={styles.username}>{comment.user.username}</div>
+          <a href={`/profile/${user.id}`}>
+            <div className={styles.username}>{comment.user.username}</div>
+          </a>
           <div className={styles.dateTimeContainer}>
             <div className={styles.timestamp}>
               {formatDateTime(comment.created_at)}
